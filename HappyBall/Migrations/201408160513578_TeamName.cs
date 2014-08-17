1dbf7c5b8c9c74c371513c1b077cd4c32bda0b83
nvolpe <nvolpe@dtsagile.com>
2014-08-17 15:05:42 -0600

    Initial Commit

---
+++ HappyBall/Migrations/201408160513578_TeamName.cs
@@ -0,0 +1,18 @@
+namespace HappyBall.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TeamName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "TeamName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "TeamName");
+        }
+    }
+}
