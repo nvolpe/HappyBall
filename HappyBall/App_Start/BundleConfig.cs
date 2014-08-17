d6b22c77ac85bdab56336805f83a75643b20c348
nvolpe <nvolpe@dtsagile.com>
2014-08-17 16:40:49 -0600

    off canvas side nav

--- HappyBall/App_Start/BundleConfig.cs
+++ HappyBall/App_Start/BundleConfig.cs
@@ -21,10 +21,12 @@ namespace HappyBall
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/jquery.navgoco.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/offcanvas.css",
                       "~/Content/site.css"));
         }
     }
